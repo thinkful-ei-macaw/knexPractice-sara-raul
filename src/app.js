e80dc746be1fa9ab5cc82843ed40792a9d4a5a9b
Sara R Mills <SaraRMills@gmail.com>
2020-03-31 09:24:03 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const app = express()
+
+const morganOption = (NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+app.use(morgan(morganOption))
+app.use(helmet())
+app.use(cors())
+
+app.use((error, req, res, next) => {
+  let message
+  if (NODE_ENV === 'production') {
+    message = 'Server error'
+  }
+  else {
+    console.error(error)
+    message = error.message
+  }
+  res.status(500).json(message)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello, boilerplate!')
+})
+
+module.exports = app
