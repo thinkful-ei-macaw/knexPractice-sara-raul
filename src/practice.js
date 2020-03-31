1cf90827b81b9cc42bb69c8e651c022e0578a2f3
Sara R Mills <SaraRMills@gmail.com>
2020-03-31 12:11:50 -0400

    pre-assignment drills

---
+++ src/practice.js
@@ -0,0 +1,87 @@
+require('dotenv').config()
+const knex = require('knex')
+
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL
+});
+
+const qry = knexInstance
+  .select('product_id', 'name', 'price', 'category')
+  .from('amazong_products')
+  .where({
+    name: 'Point of view gun'
+  })
+  .first()
+  .toQuery()
+
+function searchByProduceName(searchTerm) {
+  knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then(result => {
+      console.log(result)
+    })
+}
+
+function paginateProducts(page) {
+  const productsPerPage = 10
+  const offset = productsPerPage * (page-1)
+  knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .limit(productsPerPage)
+    .offset(offset)
+    .then(result => {
+      console.log(result)
+    })
+}
+
+function getProductsWithImages() {
+  knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .whereNotNull('image')
+    .then(result => {
+      console.log(result)
+    })
+}
+
+function mostPopularVideosForDays(days) {
+  knexInstance
+    .select('video_name', 'region')
+    .count('date_viewed AS views')
+    .where(
+      'date_viewed', 
+      '>',
+      knexInstance.raw(`now() - '?? days'::INTERVAL`, days)
+    )
+    .from('whopipe_video_views')
+    .groupBy('video_name', 'region')
+    .orderBy([
+      { column: 'region', order: 'ASC' },
+      { column: 'views', order: 'DESC'},
+    ])
+    .then(result => {
+      console.log(result)
+    })
+}
+
+mostPopularVideosForDays(30)
+
+//getProductsWithImages()
+//paginateProducts(1)
+
+//searchByProduceName('holo')
+
+
+/*
+this goes in the .env :
+
+NODE_ENV=development
+PORT=8000
+DB_URL="postgresql://dunder_mifflin@localhost/knex-practice"
+
+*/
